ca6f7b0452bd0a640cbdd68a89f51b3e238231d3
kauaDev22 <eukauapro@gmail.com>
2022-08-09 18:53:40 -0300

    modificação final 'null&undefined.js'

--- null&undefined.js
+++ null&undefined.js
@@ -4,3 +4,18 @@ console.log(valor);
 valor = null;
 
 console.log(valor); //  ausência de valor
+//console.log(valor.toString()); // erro! Cannot read property 'toString' of null
+
+const produto = {};
+console.log(produto.preco);
+console.log(produto);
+
+produto.preco = 3.5;
+console.log(produto);
+
+produto.preco = undefined; // evite atribuir undefined
+console.log(!!produto.preco);
+console.log(produto);
+
+produto.preco = null // produto sem preço 
+console.log(!!produto.preco);

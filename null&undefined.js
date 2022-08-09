c5eb20c4fdf023a830be8b17db382727069e6e17
kauaDev22 <eukauapro@gmail.com>
2022-08-09 17:53:22 -0300

    fundamentosJS - adicionei 'null&undefines.js' e modifiquei 'obeject.js'

---
+++ null&undefined.js
@@ -0,0 +1,6 @@
+let valor; // não inicializada
+console.log(valor);
+
+valor = null;
+
+console.log(valor); //  ausência de valor
--- obeject.js
+++ obeject.js
@@ -1,1 +1,15 @@
-const product = {};
+const product1 = {};
+
+product1.name = "peraphone ";
+
+product1.price = 3000.0;
+
+product1["descontão legal"] = 0.5; // Avoid atributtes with space
+
+console.log(product1);
+const product2 = {
+  nome: "Camisa Polo",
+  preço: 79.9,
+};
+
+console.log(product2);
